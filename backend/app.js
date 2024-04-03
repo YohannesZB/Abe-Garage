7f1df7a688af064a533e718f34ba33faeed14e7b
Kedus1922 <152933553+Kedus1922@users.noreply.github.com>
2024-04-02 20:07:14 -0400

    abe garage initial commit

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE" // what matters here is that OPTIONS is present
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE" 
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
